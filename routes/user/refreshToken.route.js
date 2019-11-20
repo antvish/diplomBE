9852ae6ecb8666351f0382f3d0b284cd6fbba9f1
antvish <batonio1862>
2019-11-20 19:50:00 +0300

    added check that refresh token was sent to server

--- routes/user/refreshToken.route.js
+++ routes/user/refreshToken.route.js
@@ -7,50 +7,60 @@ const jwt = require('jsonwebtoken');
 const config = require('../../config.js');
 const fs = require('fs');
 
-const publicKEY  = fs.readFileSync('./public.key', 'utf8');
+const publicKEY = fs.readFileSync('./public.key', 'utf8');
 
 router.post('/refresh-token', (req, res, next) => {
         let refreshToken = req.cookies['refresh_token'];
         let userId = req.body.userId === undefined ? 1 : req.body.userId;
-        User
-            .getUserById(userId)
-            .then(user => {
-                if (refreshToken === user.refresh_token) {
-                    jwt.verify(refreshToken, publicKEY, config.jwtConfig.accessToken, (err, decoded) => {
-                        if (Date.now() < decoded.exp * 1000) {
-                            tokenGenerateHelper
-                                .generateTokenPair(user.login)
-                                .then(token => {
-                                    User
-                                        .updateUserById(user.id, {
-                                            refresh_token: token.refreshToken,
-                                            access_token: token.accessToken
-                                        });
-                                    res
-                                        .cookie('token', token.accessToken, config.cookieConf.accessToken)
-                                        .cookie('refresh_token', token.refreshToken, config.cookieConf.refreshToken)
-                                        .json({
-                                            timestamp: Date.now()
-                                        });
-                                });
-                        } else {
-                            return res
-                                .status(401)
-                                .json({
-                                    error: errors.INVALID_TOKEN,
-                                    timestamp: Date.now()
-                                });
-                        }
-                    });
-                } else {
-                    return res
-                        .status(401)
-                        .json({
-                            error: errors.INVALID_TOKEN,
-                            timestamp: Date.now()
+        if (refreshToken) {
+            User
+                .getUserById(userId)
+                .then(user => {
+                    if (refreshToken === user.refresh_token) {
+                        jwt.verify(refreshToken, publicKEY, config.jwtConfig.accessToken, (err, decoded) => {
+                            if (Date.now() < decoded.exp * 1000) {
+                                tokenGenerateHelper
+                                    .generateTokenPair(user.login)
+                                    .then(token => {
+                                        User
+                                            .updateUserById(user.id, {
+                                                refresh_token: token.refreshToken,
+                                                access_token: token.accessToken
+                                            });
+                                        res
+                                            .cookie('token', token.accessToken, config.cookieConf.accessToken)
+                                            .cookie('refresh_token', token.refreshToken, config.cookieConf.refreshToken)
+                                            .json({
+                                                timestamp: Date.now()
+                                            });
+                                    });
+                            } else {
+                                return res
+                                    .status(401)
+                                    .json({
+                                        error: errors.INVALID_TOKEN,
+                                        timestamp: Date.now()
+                                    });
+                            }
                         });
-                }
-            });
+                    } else {
+                        return res
+                            .status(401)
+                            .json({
+                                error: errors.INVALID_TOKEN,
+                                timestamp: Date.now()
+                            });
+                    }
+                });
+        } else {
+            return res
+                .status(401)
+                .json({
+                    error: errors.INVALID_TOKEN,
+                    timestamp: Date.now()
+                });
+        }
+
     }
 );
 
