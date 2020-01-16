ed7fbed2fc83c0a1261a28bcdcb9f9db95c93066
antvish <batonio1862>
2020-01-16 17:44:50 +0300

    refactored download method added upload method on the frontend

--- routes/document/download.route.js
+++ routes/document/download.route.js
@@ -1,24 +1,31 @@
 const express = require('express');
 const errors = require('../../helpers/errors');
+var path = require('path');
+var mime = require('mime');
+var fs = require('fs');
 
 const CUR_DIR = process.cwd();
 const router = express.Router();
 
-router.get('/download', (req, res) => {
+router.post('/download', (req, res) => {
     const file = `${CUR_DIR}/uploadDir/${req.body.fileName}`;
-    res.download(file, `${req.body.fileName}`, (err) => {
-        if (err) {
-            res
-                .json({
-                    error: errors.DOWNLOAD_ERR,
-                    timestamp: Date.now()
-                })
-                .status(500);
-        } else {
-            res
-                .status(200);
-        }
-    });
+
+    const filename = path.basename(file);
+    const mimetype = mime.getType(file);
+
+    res.setHeader('Content-disposition', 'attachment; filename=' + filename);
+    res.setHeader('Content-type', mimetype);
+
+    const filestream = fs.createReadStream(file);
+    filestream.pipe(res);
+    filestream.on('error', function(err) {
+        res
+            .json({
+                error: errors.DOWNLOAD_ERR,
+                timestamp: Date.now()
+            })
+            .status(500);
+    })
 });
 
 module.exports = router;
--- views/script/index.js
+++ views/script/index.js
@@ -102,89 +102,105 @@ window.onload = () => {
     function onUpload(event) {
         event.preventDefault();
 
-        const binaryFile = document.getElementById('input-upload').files[0];
-        if (binaryFile) {
-            calculateMd5(binaryFile);
-        } else {
-            alert('Не выбран файл');
+        const file = document.getElementById('input-upload').files[0];
+        if (!file) {
+            return alert('Не выбран файл');
         }
+        const fileReader = new FileReader();
 
-        function calculateMd5(blob) {
-            const reader = new FileReader();
-            reader.readAsBinaryString(blob);
-            reader.onloadend = function () {
-                const hash = CryptoJS.MD5(CryptoJS.enc.Latin1.parse(reader.result)).toString();
-                const formData = new FormData();
-                formData.append('document', binaryFile);
-                formData.append('hash', hash);
+        fileReader.onloadend = () => {
+            const fileMd5Hash = CryptoJS.MD5(CryptoJS.enc.Latin1.parse(fileReader.result)).toString();
+            const formData = new FormData();
+            formData.append('document', file);
+            formData.append('hash', fileMd5Hash);
 
-                return axiosInstance({
-                    method: 'POST',
-                    url: 'document/upload',
-                    data: formData,
+            return axiosInstance({
+                method: 'POST',
+                url: 'document/upload',
+                data: formData,
+                timeout: 10000,
+            })
+                .then(response => {
+                    switch (response.data.message) {
+                        case 'File hash was updated':
+                            alert('MD5 хэш файла успешно обновлен');
+                            break;
+
+                        case 'File was successfully uploaded':
+                            alert('Файл успешно загружен');
+                            break;
+
+                        default:
+                            alert('Что-то пошло не так');
+                            break;
+                    }
+
                 })
-                    .then(response => {
-                        switch (response.data.message) {
-                            case 'File hash was updated':
-                                alert('MD5 хэш файла успешно обновлен');
-                                break;
+                .catch(error => {
+                    console.error(error);
 
-                            case 'File was successfully uploaded':
-                                alert('Файл успешно загружен');
-                                break;
-                        }
-                    })
-                    .catch(error => {
-                        console.error(error);
+                    let errorText = 'Что-то пошло не так';
+                    switch (error.response.status) {
+                        case 400:
+                            errorText = 'Укажите файл';
+                            break;
 
-                        let errorText = 'Что-то пошло не так';
-                        switch (error.response.status) {
-                            case 400:
-                                errorText = 'Укажите файл';
-                                break;
+                        case 401:
+                            errorText = 'Вы не авторизованы, обновите страницу';
+                            break;
 
-                            case 401:
-                                errorText = 'Вы не авторизованы, обновите страницу';
-                                break;
+                        case 406:
+                            errorText = 'Неверный хэш файла';
+                            break;
+                    }
 
-                            case 406:
-                                errorText = 'Неверный хэш файла';
-                                break;
-                        }
+                    alert(errorText);
+                });
+        };
 
-                        alert(errorText);
-                    });
-            }
-        }
+        return fileReader.readAsBinaryString(file);
     }
 
     function onDownload(event) {
         event.preventDefault();
 
-        /*const tmpElement = document.createElement('a');
-        tmpElement.setAttribute('href', 'data:text/plain;charset=utf-8,' + encodeURIComponent(text));
-        tmpElement.setAttribute('download', event.target.elements.filename.value);
-        tmpElement.style.display = 'none';
-        document.body.appendChild(tmpElement);
-        tmpElement.click();
-        document.body.removeChild(tmpElement);*/
+        const fileName = event.target.elements.filename.value;
 
-        /*return downloadFile(`https://37.204.57.169:8080:document/download?fileName=${event.target.elements.filename.value}`)
+        axiosInstance({
+            url: 'document/download',
+            responseType: 'blob',
+            method: 'POST',
+            timeout: 10000,
+            data: {
+                fileName,
+            },
+        })
             .then(response => {
-                console.log(response);
+                const url = window.URL.createObjectURL(new Blob([response.data]));
+
+                const link = document.createElement('a');
+                link.style.display = 'none';
+                link.href = url;
+                link.download = fileName;
+                document.body.appendChild(link);
+                link.click();
+                window.URL.revokeObjectURL(url);
             })
             .catch(error => {
                 console.error(error);
 
                 let errorText = 'Что-то пошло не так';
                 switch (error.response.status) {
+                    case 400:
+                        errorText = 'Файл не найден!';
+                        break;
+
                     case 401:
                         errorText = 'Вы не авторизованы, обновите страницу';
                         break;
                 }
 
                 alert(errorText);
-            });*/
+            });
     }
-}
-;
+};
